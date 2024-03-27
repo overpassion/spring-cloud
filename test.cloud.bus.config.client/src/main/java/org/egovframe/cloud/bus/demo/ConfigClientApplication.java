85eb278418cc30a0292332bd95f19864ac93825f
U-EGOV-YHSHIN\YHSHIN-EGOV <comghost@paran.com>
2024-03-27 16:33:08 +0900

    Spring Cloud Bus Example

---
+++ test.cloud.bus.config.client/src/main/java/org/egovframe/cloud/bus/demo/ConfigClientApplication.java
@@ -0,0 +1,10 @@
+package org.egovframe.cloud.bus.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ test.cloud.bus.config.server/src/main/java/org/egovframe/cloud/bus/demo/ConfigController.java
@@ -0,0 +1,16 @@
+package org.egovframe.cloud.bus.demo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${message: Default Message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
+    }
+}
